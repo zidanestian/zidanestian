99fc305fd32ff1ecd3a0aac3eaa168d73b048658
zidanestian <estianzidan@gmail.com>
2021-03-25 08:14:28 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body style="background-color: #FFA07A">
+	<center>
+		<p style="font-size: 10px; font-family: IMPACT; color: white">Chrisan Soft Store</p>
+	<div style="background-color: #FF00FF; height: 20px"></div>
+	<img src="123.jpg" style="width: 550px; height: 250px">
+	</center>
+	<div style="background-color: #4682B4">
+		<marquee scrolldelay="12" direction="right" style="font-family: BODONI MT BLACK; font-size: 5px; color: #FF00FF">Get yout free Windows 10 upgrade!</marquee>
+	</div>
+	<div>
+		<p>
+			<i style="margin-left: 40px"><b>Windows 10 Home Version:</b></i><br>
+			<ol type="A" start="7">
+				<li>1809</li>
+				<li>1903</li>
+				<li>1909</li>
+				<li>2004</li>
+			</ol>
+		</p>
+		<p>
+			<u style="margin-left: 40px"><b>Windows 10 STLC Version</b></u>
+			
+			<ul type="disc">
+				<li>1507</li>
+				<li>1607</li>
+				<li>1809</li>
+			</ul>
+		</p>
+	</div>
+	<center>
+		<span><b>Compare Windows 10</b></span>
+		<table border style="background-color: #87CEEB">
+			<tr style="background-color: #4169E1">
+				<td align="center" rowspan="2">Feature</td>
+				<td align="center" colspan="3">Windows 10</td>
+			</tr>
+			<tr style="background-color: #1E90FF">
+				<td align="center">Home</td>
+				<td align="center">Pro</td>
+				<td align="center">Enterprise(LTSC)</td>
+			</tr>
+			<tr>
+				<td>Firewall and network protection</td>
+				<td>Support</td>
+				<td>Support</td>
+				<td>Support</td>
+			</tr>
+			<tr>
+				<td>Microsoft Store fo Business</td>
+				<td>Not Support</td>
+				<td>Support</td>
+				<td>Support</td>
+			</tr>
+			<tr>
+				<td>Microsoft Application virtualization (App-V)</td>
+				<td>Not Support</td>
+				<td>Not Support</td>
+				<td>Support</td>
+			</tr>
+		</table>
+	</center>
+
+	<table>
+		<tr>
+			<td>Name</td>
+			<td>:</td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td>Comment</td>
+			<td>:</td>
+			<td><textarea></textarea></td>
+		</tr>
+		<tr>
+			<td colspan="3"> 
+				<button>Send</button>
+				<button>Cancel</button>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
